0b6202993ebe395ac7ca81b1de3686e626ac0e43
Farit Teran <99695702+faritteranacosta@users.noreply.github.com>
2025-11-28 17:36:16 -0500

    Update config.js

--- assets/config/config.js
+++ assets/config/config.js
@@ -1,7 +1,7 @@
 // Configuración global
 const CONFIG = {
-    API_URL: 'https://backend-nearby-86jj.onrender.com/api',
-    SOCKET_URL: 'https://backend-nearby-86jj.onrender.com'
+    API_URL: 'https://nearby-backend-95xa.onrender.com/api',
+    SOCKET_URL: 'https://nearby-backend-95xa.onrender.com'
 };
 // Exportar para módulos
 export default CONFIG;
